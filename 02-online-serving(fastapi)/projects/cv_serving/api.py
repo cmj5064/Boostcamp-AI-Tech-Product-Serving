3813318702918123cd799887a804c4467d330e2c
cmj5064 <duttu5064@naver.com>
2024-03-06 12:14:35 +0900

    feat: fastapi 기본과제 완료

--- 02-online-serving(fastapi)/projects/cv_serving/api.py
+++ 02-online-serving(fastapi)/projects/cv_serving/api.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, status, UploadFile
+from fastapi import APIRouter, HTTPException, status, UploadFile, File
 from pydantic import BaseModel
 from sqlmodel import Session
 from PIL import Image
-from torchvision.transforms import transforms
+from torchvision.transforms import transforms, ToTensor, Normalize
+from io import BytesIO
 
 from database import PredictionResult, engine
 from dependencies import get_model
@@ -17,12 +18,37 @@ class PredictionResponse(BaseModel):
 
 # FastAPI 경로
 @router.post("/predict")
-def predict(file: UploadFile) -> PredictionResponse:
-    # TODO: 이미지 파일과 사이즈가 맞는지 검증
+def predict(file: UploadFile = File(...)) -> PredictionResponse:
+    # TODO: 이미지 파일과 사이즈가 맞는지 검증 (v)
+    if file.content_type != "image/jpeg":
+        raise HTTPException(
+            detail=f"Wrong format: {file.content_type}", status_code=status.HTTP_404_NOT_FOUND
+        )
+    if not file.size:
+        raise HTTPException(
+            detail=f"{file.size} file size", status_code=status.HTTP_404_NOT_FOUND
+        )
+    image = Image.open(BytesIO(bytearray(file.file.read())))
 
-    # TODO: 모델 추론
+    # TODO: 모델 추론 (v)
+    transform = transforms.Compose([
+        ToTensor(),
+        Normalize(mean=(0.548, 0.504, 0.497), std=(0.237, 0.247, 0.246))
+    ])
+    
+    image = transform(image).unsqueeze(dim=0)
+    image = image.to("cpu")
 
-    # TODO: 결과를 데이터베이스에 저장 (PredictionResult 사용)
+    model = get_model()
+    pred = model(image)
+    pred = int(pred.argmax(dim=-1).cpu().numpy())
 
-    # TODO: 응답하기
-    return PredictionResponse(...)
+    # TODO: 결과를 데이터베이스에 저장 (PredictionResult 사용) (v)
+    prediction_result = PredictionResult(result=pred)
+    with Session(engine) as session:
+        session.add(prediction_result)
+        session.commit()
+        session.refresh(prediction_result)
+
+    # TODO: 응답하기 (v)
+    return PredictionResponse(id=prediction_result.id, result=pred)
